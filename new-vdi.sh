77b05a7df7c5c34a67cb5436706d1dcf2753f986
Ludovic MENTHILLER <lumenthi@e1r7p4.clusters.42paris.fr>
2022-09-04 16:35:07 +0200

    :children_crossing: Add logs for vdi script

--- new-vdi.sh
+++ new-vdi.sh
@@ -1,4 +1,14 @@
 # ssh-add ~/.ssh/ft_linux
+echo "[*] Removing old vdi..."
 rm ~/sgoinfre/lumenthi-LFS.vdi
+echo "[*] Removed old vdi !"
+
+echo "[*] Copying template vdi..."
 cp ~/sgoinfre/ft_linux_backups/lumenthi-LFS.vdi ~/sgoinfre/lumenthi-LFS.vdi
+echo "[*] Copied template vdi !"
+
+echo "[*] Generating checksum..."
 shasum < ~/sgoinfre/lumenthi-LFS.vdi > shasum
+echo "[*] Generated checksum !"
+
+cat shasum
